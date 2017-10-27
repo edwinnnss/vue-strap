bf3a7c36dc27541d856a38844c58e0a954304e69
Edwin Setiawan <edwinnnss@outlook.com>
2017-10-27 14:27:48 +0700

    fix events

--- src/Dropdown.vue
+++ src/Dropdown.vue
@@ -1,61 +1,92 @@
 <template>
-  <div :is="isLi?'li':'div'" v-click-outside="blur" @click="$emit('click')" @mouseover="$emit('mouseover')" @mouseout="$emit('mouseout')"
-    :class="[{open:show,disabled:disabled,dropdown:isLi,'input-group-btn':inInput,'btn-group':!isLi&&!inInput}]"
-  >
+  <div :is="isLi?'li':'div'" v-click-outside="blur" @click="$emit('click')" @mouseover="$emit('mouseover')" @mouseout="$emit('mouseout')" :class="[{open:show,disabled:disabled,dropdown:isLi,'input-group-btn':inInput,'btn-group':!isLi&&!inInput}]">
     <slot name="before"></slot>
-    <a v-if="isLi" role="button" :class="['dropdown-toggle',buttonSize,{disabled:disabled}]"
-      @keyup.esc="show = false"
-    >
+    <a v-if="isLi" role="button" :class="['dropdown-toggle',buttonSize,{disabled:disabled}]" @keyup.esc="show = false">
       <slot name="button">{{ text }}</slot>
       <span class="caret"></span>
     </a>
-    <button v-else type="button" :class="['btn btn-' + type,buttonSize,'dropdown-toggle']" :disabled="disabled"
-      @keyup.esc="show = false"
-    >
+    <button v-else type="button" :class="['btn btn-' + type,buttonSize,'dropdown-toggle']" :disabled="disabled" @keyup.esc="show = false">
       <slot name="button">{{ text }}</slot>
       <span class="caret"></span>
     </button>
     <slot name="dropdown-menu">
-      <ul class="dropdown-menu"><slot></slot></ul>
+      <ul class="dropdown-menu">
+        <slot></slot>
+      </ul>
     </slot>
   </div>
 </template>
 <script>
-import $ from './utils/NodeList.js'
-import ClickOutside from './directives/ClickOutside.js'
+import $ from './utils/NodeList.js';
+import ClickOutside from './directives/ClickOutside.js';
 
 export default {
   directives: {
     ClickOutside
   },
   props: {
-    disabled: {type: Boolean, default: false},
-    size: {type: String, default: null},
-    text: {type: String, default: null},
-    type: {type: String, default: 'default'},
-    value: {type: Boolean, default: false}
+    disabled: { type: Boolean, default: false },
+    size: { type: String, default: null },
+    text: { type: String, default: null },
+    type: { type: String, default: 'default' },
+    value: { type: Boolean, default: false }
   },
-  data () {
-    var show = this.value
-    return {show}
+  data() {
+    var show = this.value;
+    return { show };
   },
   watch: {
-    show (val) { this.$emit('input', val) },
-    value (val) { this.show = val }
+    show(val) {
+      this.$emit('input', val);
+    },
+    value(val) {
+      this.show = val;
+    }
   },
   computed: {
-    buttonSize () { return ~['lg', 'sm', 'xs'].indexOf(this.size) ? 'btn-' + this.size : '' },
-    inInput () { return this.$parent._input },
-    isLi () { return this.$parent._isTabs || this.$parent._navbar || this.$parent.menu },
-    menu () { return !this.$parent || this.$parent.navbar },
-    slots () { return this._slotContents },
-    submenu () { return this.$parent && (this.$parent.menu || this.$parent.submenu) }
+    buttonSize() {
+      return ~['lg', 'sm', 'xs'].indexOf(this.size) ? 'btn-' + this.size : '';
+    },
+    inInput() {
+      return this.$parent._input;
+    },
+    isLi() {
+      return this.$parent._isTabs || this.$parent._navbar || this.$parent.menu;
+    },
+    menu() {
+      return !this.$parent || this.$parent.navbar;
+    },
+    slots() {
+      return this._slotContents;
+    },
+    submenu() {
+      return this.$parent && (this.$parent.menu || this.$parent.submenu);
+    }
   },
   methods: {
-    blur () { this.show = false },
-    toggle () {
-      if (!this.disabled) { this.show = !this.show }
+    blur() {
+      this.show = false;
+    },
+    toggle() {
+      if (!this.disabled) {
+        this.show = !this.show;
+      }
+    },
+    click() {
+      if (!this.disabled) {
+        this.$emit('click');
+      }
+    },
+    mouseover() {
+      if (!this.disabled) {
+        this.$emit('mouseover');
+      }
+    },
+    mouseout() {
+      if (!this.disabled) {
+        this.$emit('mouseout');
+      }
     }
   }
-}
+};
 </script>
